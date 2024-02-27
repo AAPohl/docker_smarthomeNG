c931749dc11210375810a60d02ffa3c7f010b118
AAPohl <82733706+AAPohl@users.noreply.github.com>
2024-02-27 23:41:01 +0100

    workaround: copy logging file

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,10 @@ if [ -f $PATH_SHNG/etc/.not_mounted ]; then
       touch $PATH_CONF/$i/.files_created
     fi
   done
+
+  ## workaround logging file missing
+  cp -u $PATH_SHNG/etc/logging.yaml.default $PATH_SHNG/etc/logging.yaml
+  
   if [ "$WARN_MOUNT_CONF" = "$DIRS_CONF" ]; then
     _print WARN: $PATH_CONF not mounted. Config files will not be permanent!
   elif [ "$WARN_MOUNT_CONF" ]; then
